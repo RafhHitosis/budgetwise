17702c23880305624d4d00c9a1fac7d50c99b42b
RafhHitosis <rafhhitosis@gmail.com>
2025-07-31 14:08:17 +0800

    feat: PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "Expense",
+        description: "Track your expenses easily with this PWA!",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: false,
     port: 5173,
